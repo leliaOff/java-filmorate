ede65ae7a0d1c2a9129efc9b8c3b3b04855961af
Elena Polukova <e.brykova@yofin.ru>
2024-05-08 13:07:43 +0400

    В ErrorHandler добавлено логирование ошибок

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -8,23 +9,27 @@ import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.response.ErrorResponse;
 
+@Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleNotFound(final NotFoundException e) {
+        log.error("NotFoundException: " + e.getMessage());
         return new ErrorResponse("Данные не найдены", e.getMessage());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleValidation(final ValidationException e) {
+        log.error("ValidationException: " + e.getMessage() + "\n" + e.getErrors().toString());
         return new ErrorResponse("Неверные параметры запроса", e.getMessage(), e.getErrors());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handle(final Throwable e) {
+        log.error("Throwable: " + e.getMessage());
         return new ErrorResponse("Произошла непредвиденная ошибка", e.getMessage());
     }
 }
