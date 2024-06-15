9eea8cba9884254c3b1ba042c4c65f4fb6d73c4b
Elena Polukova <e.brykova@yofin.ru>
2024-06-15 10:47:26 +0400

    Схема БД

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,62 @@
+CREATE TABLE IF NOT EXISTS genres
+(
+    id bigint auto_increment,
+    name varchar NOT NULL,
+    CONSTRAINT genres_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS ratings
+(
+    id bigint auto_increment,
+    name varchar NOT NULL,
+    CONSTRAINT ratings_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS films
+(
+    id bigint auto_increment,
+    name varchar NOT NULL,
+    description text,
+    release_date date,
+    duration integer,
+    genre_id bigint NOT NULL,
+    rating_id bigint NOT NULL,
+    created_at timestamp DEFAULT NOW() NOT NULL,
+    CONSTRAINT films_pk PRIMARY KEY (id),
+    CONSTRAINT films_genres_id_fk FOREIGN KEY (genre_id) REFERENCES GENRES (id),
+    CONSTRAINT films_ratings_id_fk FOREIGN KEY (rating_id) REFERENCES RATINGS (id)
+);
+
+CREATE TABLE IF NOT EXISTS users
+(
+    id bigint auto_increment,
+    email varchar NOT NULL,
+    login varchar NOT NULL,
+    name varchar NOT NULL,
+    birthday date,
+    created_at timestamp DEFAULT NOW() NOT NULL,
+    CONSTRAINT users_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS user_follows
+(
+    id bigint auto_increment,
+    following_user_id bigint NOT NULL,
+    followed_user_id bigint NOT NULL,
+    state smallint NOT NULL,
+    created_at timestamp DEFAULT NOW() NOT NULL,
+    CONSTRAINT user_follows_pk PRIMARY KEY (id),
+    CONSTRAINT user_follows_following_users_id_fk FOREIGN KEY (following_user_id) REFERENCES USERS (id),
+    CONSTRAINT user_follows_followed_users_id_fk FOREIGN KEY (followed_user_id) REFERENCES USERS (id)
+);
+
+CREATE TABLE IF NOT EXISTS film_user_ratings
+(
+    id bigint auto_increment,
+    user_id bigint NOT NULL,
+    film_id bigint NOT NULL,
+    created_at timestamp DEFAULT NOW() NOT NULL,
+    CONSTRAINT film_user_ratings_pk PRIMARY KEY (id),
+    CONSTRAINT film_user_ratings_users_id_fk FOREIGN KEY (user_id) REFERENCES USERS (id),
+    CONSTRAINT film_user_ratings_films_id_fk FOREIGN KEY (film_id) REFERENCES FILMS (id)
+);
